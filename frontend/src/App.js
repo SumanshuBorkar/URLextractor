c9274bdbd13707ec740283fadacf590112bba167
Sumanshu <101553319+SumanshuBorkar@users.noreply.github.com>
2025-06-25 17:24:42 +0530

    Update App.js
    changedthe url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ function App() {
     setResults([]);
 
     try {
-      const res = await axios.post('ur-lextractor-naxe-nthlau77t-sumanshuborkars-projects.vercel.app/api/scrape', { url, query });
+      const res = await axios.post('https://ur-lextractor-naxe.vercel.app/api/scrape', { url, query });
       const matches = res.data.results;
       console.log(matches, "this is the response")
 
